9cdc594de534181cb92dba647c66c3118a8ba027
wooddie <n1ght.d1ck1ns@gmail.com>
2025-08-19 22:36:04 +0500

    exercise 7kyu completed

---
+++ digital_cypher.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <iostream>
+#include <cassert>
+
+class Kata
+{
+public:
+    static std::vector<int> Encode(std::string str, int n)
+    {
+        std::vector<int> list_key;
+        while (n > 0)
+        {
+            int digit = n % 10;
+            list_key.push_back(digit);
+            n /= 10;
+        }
+
+        std::reverse(list_key.begin(), list_key.end());
+        int current_index = 0;
+        std::vector<int> num;
+
+        for(size_t i = 0; i < str.size(); ++i)
+        {
+            num.push_back((str[i] - 'a' + 1) + list_key[current_index]);
+            current_index = (current_index + 1) % list_key.size();
+        }
+
+        return num;
+    }
+};
+
+int main()
+{
+    assert(Kata::Encode("scout", 1939) == (std::vector<int>({20, 12, 18, 30, 21})));
+    assert(Kata::Encode("masterpiece", 1939) == (std::vector<int>({14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8})));
+
+    std::cout << "all tests passed!" << std::endl;
+}

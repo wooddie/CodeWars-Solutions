edc022e5c561dc93f2addc805b72da9370755fdf
wooddie <n1ght.d1ck1ns@gmail.com>
2025-08-20 13:41:02 +0500

    exercise 7kyu completed

---
+++ GA-DE-RY-PO-LU-KI_cypher.cpp
@@ -0,0 +1,68 @@
+#include <string>
+#include <cassert>
+#include <unordered_map>
+#include <iostream>
+
+std::unordered_map<char, char> gaderypoluki = {
+    {'A', 'G'}, {'G', 'A'},
+    {'D', 'E'}, {'E', 'D'},
+    {'Y', 'R'}, {'R', 'Y'},
+    {'O', 'P'}, {'P', 'O'},
+    {'L', 'U'}, {'U', 'L'},
+    {'I', 'K'}, {'K', 'I'},
+
+    {'a', 'g'}, {'g', 'a'},
+    {'d', 'e'}, {'e', 'd'},
+    {'y', 'r'}, {'r', 'y'},
+    {'o', 'p'}, {'p', 'o'},
+    {'l', 'u'}, {'u', 'l'},
+    {'i', 'k'}, {'k', 'i'}
+};
+
+std::string encode(const std::string &str)
+{
+    std::string result;
+    for (char c : str)
+    {
+        if (gaderypoluki[c])
+        {
+            result += gaderypoluki[c];
+        }
+        else
+        {
+            result += c;
+        }
+    }
+
+    return result;
+}
+
+std::string decode(const std::string &str)
+{
+    std::string result;
+    for (char c : str)
+    {
+        if (gaderypoluki[c])
+        {
+            result += gaderypoluki[c];
+        }
+        else
+        {
+            result += c;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+
+    assert(encode("Ala has a cat") == ("Gug hgs g cgt"));
+    assert(decode("Gug hgs g cgt") == ("Ala has a cat"));
+    assert(encode("ABCD") == ("GBCE"));
+    assert(decode("GBCE") == ("ABCD"));
+    assert(encode("gaderypoluki") == ("agedyropulik"));
+    assert(decode("agedyropulik") == ("gaderypoluki"));
+    std::cout << "all tests passed!" << std::endl;
+
+}

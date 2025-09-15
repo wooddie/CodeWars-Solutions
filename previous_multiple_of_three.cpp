4a1d8fa5e0aabfbd1507cb00dcec1496bf9bfda9
wooddie <n1ght.d1ck1ns@gmail.com>
2025-09-15 16:34:13 +0500

    exercise 7kyu completed

--- previous_multiple_of_three.cpp
+++ previous_multiple_of_three.cpp
@@ -3,6 +3,18 @@
 
 int prev_mult_of_three(int n)
 {
+    while (n > 0)
+    {
+        if (n % 3 != 0)
+        {
+            n = n / 10;
+        }
+        else
+        {
+            return n;
+        }
+    }
+
     return -1;
 }
 
@@ -19,10 +31,10 @@ int main()
     return 0;
 }
 
-// Given a positive integer n: 0 < n < 1e6, remove the last digit until 
+// Given a positive integer n: 0 < n < 1e6, remove the last digit until
 // you're left with a number that is a multiple of three.
 
-// Return n if the input is already a multiple of three, and if no such 
+// Return n if the input is already a multiple of three, and if no such
 // number exists, return null, a similar empty value, or -1.
 
 // Examples

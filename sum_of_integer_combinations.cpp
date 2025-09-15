95e55f3de605b5040892aacfb1d9e118ecf8c3b0
wooddie <n1ght.d1ck1ns@gmail.com>
2025-09-15 11:53:52 +0500

    add details

--- sum_of_integer_combinations.cpp
+++ sum_of_integer_combinations.cpp
@@ -26,3 +26,14 @@ int main()
 
     return 0;
 }
+
+// Consider the array [3,6,9,12]. If we generate all the combinations with 
+// repetition that sum to 12, we get 5 combinations: 
+// [12], [6,6], [3,9], [3,3,6], [3,3,3,3]. The length of the sub-arrays 
+// (such as [3,3,3,3] should be less than or equal to the length of the initial 
+// array ([3,6,9,12]).
+
+// Given an array of positive integers and a number n, count all combinations 
+// with repetition of integers that sum to n. For example:
+
+// find([3,6,9,12],12) = 5.

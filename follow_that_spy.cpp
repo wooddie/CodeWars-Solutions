c7ed3a1855f9c573659dfd50560f63a0937fe931
wooddie <n1ght.d1ck1ns@gmail.com>
2025-09-18 15:23:27 +0500

    exercise 6kyu completed

--- follow_that_spy.cpp
+++ follow_that_spy.cpp
@@ -1,50 +1,75 @@
 #include <cassert>
 #include <iostream>
+#include <set>
 #include <vector>
 #include <string>
 #include <utility>
 
 std::string find_routes(const std::vector<std::pair<std::string, std::string>> &routes)
 {
-    std::string result;
+    std::set<std::string> first, second;
+    std::string start, result;
 
-    for (int i = 0; i < routes.size() - 1; ++i)
+    for (size_t i = 0; i < routes.size(); ++i)
     {
-        while (true)
-        {
-
-        }
-        
+        first.insert(routes[i].first);
+        second.insert(routes[i].second);
     }
-    
+
+    for (const auto &it : first)
+    {
+        if (second.count(it) == 0)
+        {
+            start = it;
+            break;
+        }
+    }
+
+    result += start;
+
+    while (true)
+    {
+        bool found = false;
+        for (std::pair<std::string, std::string> p : routes)
+        {
+            if (start == p.first)
+            {
+                result += ", " + p.second;
+                start = p.second;
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            break;
+    }
+
     return result;
 }
 
 int main()
 {
     std::vector<std::pair<std::string, std::string>> in = {{"MNL", "TAG"}, {"CEB", "TAC"}, {"TAG", "CEB"}, {"TAC", "BOR"}};
-    std::cout << find_routes(in) << std::endl;
-    //assert(find_routes(in) == ("MNL, TAG, CEB, TAC, BOR"));
+    assert(find_routes(in) == ("MNL, TAG, CEB, TAC, BOR"));
 
     in = {{"Chicago", "Winnipeg"}, {"Halifax", "Montreal"}, {"Montreal", "Toronto"}, {"Toronto", "Chicago"}, {"Winnipeg", "Seattle"}};
-    //std::cout << find_routes(in) << std::endl;
-    //assert(find_routes(in) == ("Halifax, Montreal, Toronto, Chicago, Winnipeg, Seattle"));
+    assert(find_routes(in) == ("Halifax, Montreal, Toronto, Chicago, Winnipeg, Seattle"));
 
     std::cout << "all tests passed!" << std::endl;
 
     return 0;
 }
 
-// We are diligently pursuing our elusive operative, Matthew Knight, who also 
-// goes by the alias Roy Miller. He employs a nomadic lifestyle to evade detection, 
-// constantly moving from one location to another, with each of his journeys following 
-// a perplexing and non-standard sequence of itineraries. Our mission is to decipher 
+// We are diligently pursuing our elusive operative, Matthew Knight, who also
+// goes by the alias Roy Miller. He employs a nomadic lifestyle to evade detection,
+// constantly moving from one location to another, with each of his journeys following
+// a perplexing and non-standard sequence of itineraries. Our mission is to decipher
 // the routes he will undertake during each of his voyages.
 
 // Task
 
-// You've been provided with an array of itinerary routes, decipher the precise 
-// destinations he will visit in the correct sequence according to his meticulously 
+// You've been provided with an array of itinerary routes, decipher the precise
+// destinations he will visit in the correct sequence according to his meticulously
 // planned itineraries.
 
 // Example

ac70657d027029ae46b1000a625c133a11b9b985
wooddie <n1ght.d1ck1ns@gmail.com>
2025-09-19 10:30:41 +0500

    exercise 7kyu completed

--- selective_fear_of_numbers.cpp
+++ selective_fear_of_numbers.cpp
@@ -4,7 +4,14 @@
 
 bool am_i_afraid(const std::string &day, int num)
 {
-    // Help me...
+    if (day == "Monday" && num == 12) return true;
+    if (day == "Tuesday" && num > 95) return true;
+    if (day == "Wednesday" && num == 34) return true;
+    if (day == "Thursday" && num == 0) return true;
+    if (day == "Friday" && num % 2 == 0) return true;
+    if (day == "Saturday" && num == 56) return true;
+    if (day == "Sunday" && (num == 666 || num == -666)) return true;
+    
     return false;
 }
 

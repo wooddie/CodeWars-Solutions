18453955099fd9fc6391425f202dc7cc71128f16
wooddie <n1ght.d1ck1ns@gmail.com>
2025-09-29 10:34:26 +0500

    5kyu task completed

--- numbers_that_are_a_power_of_their_sum_of_digits.cpp
+++ numbers_that_are_a_power_of_their_sum_of_digits.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
 #include <cassert>
 
+unsigned long sumDigits(unsigned long num)
+{
+    unsigned long sum = 0;
+
+    while (num > 0)
+    {
+        unsigned long temp = num % 10;
+        sum += temp;
+        num /= 10;
+    }
+
+    return sum;
+}
+
+unsigned long power(unsigned long base, unsigned long exp)
+{
+    unsigned long result = 1;
+    for (unsigned long k = 0; k < exp; ++k)
+    {
+        result *= base;
+    }
+    return result;
+}
+
 unsigned long powerSumDigTerm(unsigned int n)
 {
-    // your code here
-    return n;
+    std::vector<unsigned long> found;
+    for (unsigned long i = 2; i < 200; ++i)
+    {
+        for (unsigned long j = 2; j < 50; ++j)
+        {
+            unsigned long s = power(i, j);
+            if (sumDigits(s) == i)
+            {
+                found.push_back(s);
+            }
+        }
+    }
+    std::sort(found.begin(), found.end());
+    found.erase(std::unique(found.begin(), found.end()), found.end());
+    return found[n - 1];
 }
 
 int main()
@@ -37,9 +74,9 @@ int main()
 // this property (not considering numbers of one digit). The next one, is 512.
 // Let's see both cases with the details
 
-// 8 + 1 = 9 and 92 = 81
+// 8 + 1 = 9 and 9^2 = 81
 
-// 512 = 5 + 1 + 2 = 8 and 83 = 512
+// 512 = 5 + 1 + 2 = 8 and 8^3 = 512
 
 // We need to make a function that receives a number as argument n and returns
 // the n-th term of this sequence of numbers.

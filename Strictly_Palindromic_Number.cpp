d9458123e0e0a10ff0b0238611492fcf5e5432e6
wooddie <n1ght.d1ck1ns@gmail.com>
2025-10-24 10:35:57 +0500

    medium leetcode task completed

---
+++ Strictly_Palindromic_Number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class Solution
+{
+public:
+    bool isStrictlyPalindromic(int n)
+    {
+        return false;
+    }
+};
+
+int main()
+{
+    Solution s1;
+
+    std::cout << std::boolalpha << s1.isStrictlyPalindromic(9) << std::endl;
+    std::cout << std::boolalpha << s1.isStrictlyPalindromic(4) << std::endl;
+
+    return 0;
+}

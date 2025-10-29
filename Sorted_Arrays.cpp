5f9aaf64567b0251d04ee2800a47ab232b8ee625
wooddie <n1ght.d1ck1ns@gmail.com>
2025-10-29 15:01:54 +0500

    6kyu task completed

---
+++ Sorted_Arrays.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+int nthSmallest(const std::vector<std::vector<int>> &arr, int n)
+{
+    std::vector<int> list;
+
+    for (const auto &a : arr)
+    {
+        for (const auto &e : a)
+        {
+            list.push_back(e);
+        }
+    }
+
+    std::sort(list.begin(), list.end());
+
+    return list[n - 1];
+}
+
+int main()
+{
+    std::cout << "all tests passed!" << std::endl;
+
+    assert(nthSmallest({{1, 5}, {2}, {4, 8, 9}}, 4) == 5);
+
+    return 0;
+}

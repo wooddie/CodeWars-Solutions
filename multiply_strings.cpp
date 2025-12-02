6d1c6fd5da5ef9c64616ea16ae4c88007c436d87
wooddie <n1ght.d1ck1ns@gmail.com>
2025-12-02 15:48:06 +0500

    medium leetcode task completed

--- multiply_strings.cpp
+++ multiply_strings.cpp
@@ -7,14 +7,31 @@ class Solution
 public:
     std::string multiply(std::string num1, std::string num2)
     {
-        std::stringstream ss1(num1);
-        long long n1;
-        ss1 >> n1;
-        std::stringstream ss2(num2);
-        long long n2;
-        ss2 >> n2;
+        if (num1 == "0" || num2 == "0")
+            return "0";
 
-        return std::to_string(n1 * n2);
+        std::vector<int> res(num1.size() + num2.size(), 0);
+
+        for (int i = num1.size() - 1; i >= 0; --i)
+        {
+            for (int j = num2.size() - 1; j >= 0; --j)
+            {
+                int d1 = num1[i] - '0';
+                int d2 = num2[j] - '0';
+
+                int sum = d1 * d2 + res[i + j + 1];
+
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        std::string ans;
+        for (int x : res)
+            if (!(ans.empty() && x == 0))
+                ans.push_back(x + '0');
+
+        return ans.empty() ? "0" : ans;
     }
 };
 

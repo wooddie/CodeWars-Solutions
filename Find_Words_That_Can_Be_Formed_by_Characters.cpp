796a4756e7fd1aef3ee2f60215b23bfe7c6feadc
wooddie <n1ght.d1ck1ns@gmail.com>
2025-12-10 10:15:13 +0500

    easy leetcode task completed

---
+++ Find_Words_That_Can_Be_Formed_by_Characters.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Solution
+{
+public:
+    int countCharacters(std::vector<std::string> &words, std::string chars)
+    {
+        std::vector<int> freqChars(26, 0);
+        for (char c : chars)
+        {
+            freqChars[c - 'a']++;
+        }
+
+        int res = 0;
+
+        for (const std::string &word : words)
+        {
+            std::vector<int> freqWord(26, 0);
+
+            for (char c : word)
+            {
+                freqWord[c - 'a']++;
+            }
+
+            bool good = true;
+            for (int i = 0; i < 26; i++)
+            {
+                if (freqWord[i] > freqChars[i])
+                {
+                    good = false;
+                    break;
+                }
+            }
+
+            if (good)
+            {
+                res += word.size();
+            }
+        }
+
+        return res;
+    }
+};
+
+int main()
+{
+    Solution s1;
+
+    std::vector<std::string> v = {"cat", "bt", "hat", "tree"};
+    std::cout << s1.countCharacters(v, "atach") << std::endl;
+
+    v = {"hello", "world", "leetcode"};
+    std::cout << s1.countCharacters(v, "welldonehoneyr") << std::endl;
+}
